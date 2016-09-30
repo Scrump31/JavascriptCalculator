f10126c15984ba5603c5d82442185c010b8771a8
Scottie Crump <scottiecrump@yahoo.com>
2016-09-29 22:36:02 -0400

    added autoprefixer and sourcemaps to Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
+//Compile SASS to CSS
 gulp.task('sass', function(){
   return gulp.src('app/scss/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+    }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css'));
 });
+
+//Watch the SASS file for chages
+gulp.task('watch', function(){
+  gulp.watch('app/scss/style.scss', ['sass']);
+});
+
+gulp.task('default',['watch']);
